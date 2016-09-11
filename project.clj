893c8d77f173e3ef6e5849419b2fd141e9e7f4b9
Alexandr G <alexandr@gavrisco.com>
2016-09-11 19:45:15 +0300

    Add primatic/schema for edn validation

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [aleph "0.4.2-alpha8"]
                  [gloss "0.2.6"]
-                 [manifold "0.1.6-alpha1"]]
+                 [manifold "0.1.6-alpha1"]
+                 [prismatic/schema "1.1.3"]]
   :main ^:skip-aot pad-broker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
