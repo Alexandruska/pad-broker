0d94e1e128f47c5adcd3bd70a06892aa9af3c33d
Alexandr G <alexandr@gavrisco.com>
2016-09-11 17:36:46 +0300

    Move server setup to the separated funtion

--- src/pad_broker/core.clj
+++ src/pad_broker/core.clj
@@ -10,22 +10,25 @@
     :default 14141
     :parse-fn #(Integer/parseInt %)
     :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ["-H" "--host HOSTNAME" "Hostname"
-    :default "localhost"]
    ["-h" "--help"]])
 
 (defn message-handler
   [s info]
   (println (str "New connection from " info)))
 
+(defn start-server
+  [port]
+  (println (str "Broker is up and listening on :" port))
+  (println "Press Ctrl + C to close the application")
+  (tcp/start-server message-handler {:host host :port port}))
+
+
 (defn -main
-  "Entry point for the broker."
+  "Entry point for the broker.
+
+  Runs the broker on given port (via cli arguments) and blocks the main thread"
   [& args]
-  (let [{{host :host port :port help? :help} :options :as opts} (parse-opts args cli-options)]
+  (let [{{port :port help? :help} :options :as opts} (parse-opts args cli-options)]
     (if help?
       (println (:summary opts))
-      (do
-        (println (str "Broker is up and running on " host ":" port))
-        (println "Press Ctrl + C to close the application")
-        (netty/wait-for-close
-          (tcp/start-server message-handler {:host host :port port}))))))
+      (netty/wait-for-close (start-server port)))))
