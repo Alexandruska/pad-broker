c2f52393b772ea972a53b70876d2b61e0381b43e
Alexandr G <alexandr@gavrisco.com>
2016-09-11 17:04:36 +0300

    Use the last version of aleph

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [aleph "0.4.1"]]
+                 [aleph "0.4.2-alpha8"]]
   :main ^:skip-aot pad-broker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
